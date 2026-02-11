1ba6c773ac29646a1a4638041b29a344ba16093c
Yehuda <yehudadarmon@gmail.com>
2026-02-11 14:40:15 +0200

    added func

--- reader.py
+++ reader.py
@@ -1,7 +1,14 @@
 import csv
 
-def load_network_traffic():
-    with open('network_traffic.log','r') as f:
-        reader = csv.reader(f)
-        return list(reader)
-    
+def load_network_traffic(path):
+    with open(path ,'r') as f:
+        reader = list(csv.reader(f))
+        return reader
+
+def external_addresses(data):
+    external_addresses_list = [i for i in data if not (i[1].startswith("192") or i[1].startswith("10"))]
+    return external_addresses_list
+
+def sensitive_port(data):
+    sensitive_port_list = [i for i in data if (i[3] == '22' or i[3] == '23' or i[3] == '3389')]
+    return sensitive_port_list
