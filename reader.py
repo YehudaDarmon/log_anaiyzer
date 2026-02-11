cded57ae5f795d0665283710025da515be6f21f8
Yehuda <yehudadarmon@gmail.com>
2026-02-11 23:23:46 +0200

    added func

--- reader.py
+++ reader.py
@@ -12,3 +12,18 @@ def external_addresses(data):
 def sensitive_port(data):
     sensitive_port_list = [i for i in data if (i[3] == '22' or i[3] == '23' or i[3] == '3389')]
     return sensitive_port_list
+
+def filter_by_size(data):
+    filter_by_size_list = [i for i in data if int(i[5]) > 5000]
+    return filter_by_size_list
+
+def package_size(data):
+    package_size_list = []
+    for i in data:
+        if int(i[5]) >= 5000:
+            i.append('LARGE')
+            package_size_list.append(i)
+        else:
+            i.append('NORMAL')
+            package_size_list.append(i)
+    return package_size_list
